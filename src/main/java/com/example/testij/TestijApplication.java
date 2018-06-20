ce474aa777e3d7fc891609e0c7910a73561afc57
Pradeep <er.spider29@gmail.com>
2018-06-20 12:13:32 +0530

    Base

---
+++ src/main/java/com/example/testij/TestijApplication.java
@@ -0,0 +1,12 @@
+package com.example.testij;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestijApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestijApplication.class, args);
+    }
+}
